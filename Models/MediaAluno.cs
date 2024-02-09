6ec6b13e5423c5e2146a84b938e8e6ab43aea601
Joselito Machado <opaguelosantos@gmail.com>
2024-02-09 14:33:37 -0300

    refactor: alterando o tipo da variavel

--- Models/MediaAluno.cs
+++ Models/MediaAluno.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("========================[ Desafio 6]========================\n");
 
             string nomeAluno, nomeMateria;
-            decimal notaUnidade1, notaUnidade2, notaUnidade3, notaUnidade4, mediaGeral;
+            double notaUnidade1, notaUnidade2, notaUnidade3, notaUnidade4, mediaGeral;
 
             try
             {
@@ -18,26 +18,26 @@
                 nomeMateria = Console.ReadLine();
 
                 Console.WriteLine($"\nDigite a nota da 1ª unidade: ");
-                notaUnidade1 = decimal.Parse(Console.ReadLine());
+                notaUnidade1 = double.Parse(Console.ReadLine());
 
                 Console.WriteLine($"\nDigite a nota da 2ª unidade: ");
-                notaUnidade2 = decimal.Parse(Console.ReadLine());
+                notaUnidade2 = double.Parse(Console.ReadLine());
 
                 Console.WriteLine($"\nDigite a nota da 3ª unidade: ");
-                notaUnidade3 = decimal.Parse(Console.ReadLine());
+                notaUnidade3 = double.Parse(Console.ReadLine());
 
                 Console.WriteLine($"\nDigite a nota da 4ª unidade: ");
-                notaUnidade4 = decimal.Parse(Console.ReadLine());
+                notaUnidade4 = double.Parse(Console.ReadLine());
 
                 mediaGeral = (notaUnidade1 + notaUnidade2 + notaUnidade3 + notaUnidade4) / 4;
 
-                if (mediaGeral >= 6.00m)
+                if (mediaGeral >= 6)
                 {
-                    Console.WriteLine($"Parabéns {nomeAluno}, você foi aprovado com a média: {mediaGeral} na matéria {nomeMateria}");
+                    Console.WriteLine($"Parabéns {nomeAluno}, você foi aprovado com a média ({mediaGeral}) na matéria de {nomeMateria}");
                 }
                 else
                 {
-                    Console.WriteLine($"{nomeAluno} você foi reprovado com a média: {mediaGeral} na matéria {nomeMateria}");
+                    Console.WriteLine($"{nomeAluno} você foi reprovado com a média ({mediaGeral}) na matéria de {nomeMateria}");
                 }
 
                 Menu.TelaMenu();
